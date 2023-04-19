3f403e65988b30f13d09fdef83d39c4589a80b7a
Alexander Voronin <aovoronin.piano@gmail.com>
2023-04-19 11:33:22 +0300

    feat: work on modal window, commit from desktop to laptop

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,39 @@
 import "./entry.scss";
 import { setSliderListeners } from "./js/utils";
 
-setSliderListeners()
+setSliderListeners();
+
+const submitBtn = document.querySelector(".form__submit-btn");
+const overlay = document.querySelector(".overlay");
+const fieldset = document.querySelector(".form__fieldset");
+const savedContent = new XMLSerializer().serializeToString(fieldset);
+console.log(savedContent);
+submitBtn.addEventListener("click", () => {
+  if (overlay instanceof HTMLDivElement) {
+    overlay.style.display = "block";
+  }
+  fieldset.classList.add("modal");
+
+  //   if (fieldset.hasChildNodes()) {
+  //     const children = fieldset.childNodes;
+  //     for (let i = 0; i < children.length; i++) {
+  //       if (children[i] instanceof HTMLElement) {
+  //         if (children[i].nodeName !== "LEGEND") {
+  //           children[i].remove();
+  //         }
+  //       }
+  //     }
+  //   }
+
+  document.body.style.position = "fixed";
+  document.body.style.top = `-2200px`;
+});
+
+overlay.addEventListener("click", () => {
+  if (overlay instanceof HTMLDivElement) {
+    overlay.style.display = "none";
+  }
+  fieldset.classList.remove("modal");
+  fieldset.innerHTML = savedContent;
+  document.body.style.position = "";
+});
